90e8eacfae857c6e95f1c6a21438f45145a29ea1
gempurbayu <aji123zz@gmail.com>
2022-07-03 02:45:21 +0700

    data fetching getstaticprops

--- pages/users/index.tsx
+++ pages/users/index.tsx
@@ -1,9 +1,41 @@
-export default function index() {
+import { useRouter } from 'next/router';
+import Layout from '../../components/layouts';
+import styles from '../../styles/Users.module.css';
+
+interface UserProps {
+
+  dataUsers: Array<any>;
+
+}
+
+export default function Users(props: UserProps) {
+
+  const { dataUsers } = props;
+  const router = useRouter();
+
   return (
-    <div>
+    <Layout>
       <h1>
         Index Users
       </h1>
-    </div>
+      {dataUsers.map(user => {
+        return (
+          <div key={user.id} onClick={() => router.push(`/users/${user.id}`)} className={styles.card}>
+          <p>{user.name}</p>
+          <p>{user.email}</p>
+          </div>
+        )
+      })}
+    </Layout>
   )
+}
+
+export async function getStaticProps() {
+  const res = await fetch('https://jsonplaceholder.typicode.com/users');
+  const dataUsers = await res.json();
+  return {
+    props: {
+      dataUsers,
+    }
+  }
 }
